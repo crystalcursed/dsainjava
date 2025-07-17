8fc5ffef90672356eda0f2b674e2351e7775f5c7
Nandan Tandel <143859943+crystalcursed@users.noreply.github.com>
2025-07-17 20:50:04 +0530

    Update longestcommonprefix.java

--- leetcode/arrays/longestcommonprefix.java
+++ leetcode/arrays/longestcommonprefix.java
@@ -1,1 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
 
+        int numberofStrings = strs.length;
+
+        for (int i = 0 ; i<strs[0].length(); ++i){
+
+            for (int j = 1 ; j < numberofStrings ; ++j){
+
+                if (strs[j].length() <= i || strs[j].charAt(i) != strs[0].charAt(i)){
+
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
